27bd6470f75f8018b2a8b8a13dc8fd21141e3747
KacperTetela <kacpertetela1@gmail.com>
2025-03-14 02:23:23 +0100

    change CORS to open for test purpose

--- src/main/java/checklistData/ChecklistInfoController.java
+++ src/main/java/checklistData/ChecklistInfoController.java
@@ -1,12 +1,10 @@
 package checklistData;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/checklists/info")
 public class ChecklistInfoController {
